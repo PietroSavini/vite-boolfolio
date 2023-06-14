ef37a2620dbb78bc97e5a73a4f301555bbbc6284
PietroSavini <savinipietro@gmail.com>
2023-06-14 23:40:23 +0200

    add schaffolding

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script >
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+      projects:[],
+    }
+  },
+  mounted(){
+    this.getProjects();
+  },
+
+  methods:{
+   
+    getProjects(){
+      axios.get('http://localhost:8000/Api/Project').then((resp) =>{
+        this.projects = resp.results;
+        console.log(results);
+      });
+    }
+
+  },
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="scss">
+
 </style>
