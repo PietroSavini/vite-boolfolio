f6bfbf3e498667f075b1ead117330542fac8db13
PietroSavini <savinipietro@gmail.com>
2023-06-15 19:45:01 +0200

    ADD: Card Component

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,39 @@
 <script >
 import axios from 'axios';
+import Card from './components/card.vue';
 
 export default{
-  data(){
-    return{
-      projects: [],
-      imgLink : "http://localhost:8000/storage/"
-    }
-  },
-  mounted(){
-    this.getProjects();
-    
-  },
-
-  methods:{
-   
-    getProjects(){
-      axios.get('http://localhost:8000/api/projects').then((resp) =>{
-        this.projects = resp.data.results;
-        console.log(this.projects);
-      });
-    }
-
-  },
-
+    data() {
+        return {
+            projects: [],
+            imgLink: "http://localhost:8000/storage/",
+            dbUrl: "http://localhost:8000"
+        };
+    },
+    mounted() {
+        this.getProjects();
+    },
+    methods: {
+        getProjects() {
+            axios.get(`${this.dbUrl}/api/projects`).then((resp) => {
+                this.projects = resp.data.results;
+                console.log(this.projects);
+            });
+        }
+    },
+    components: { Card }
 }
 </script>
 
 <template>
   <main>
     <div class="container py-4">
-    <div class="row">
-      <div v-for="project in projects" class="col-md-6 col-xl-4 col-sm-12 d-flex justify-content-center ">
-        <div class="card mb-3" style="width: 18rem;">
-          <img v-if="project.image !== null " class="card-img-top" :src="`${imgLink}${project.image}`" :alt="project.title">
-          <div class="card-body">
-            <h5 class="card-title">{{project.title}}</h5>
-            <p class="card-text">{{ project.description }}</p>
-            <a href="#" class="btn btn-primary">vedi progetto</a>
-          </div>
+      <div class="row">
+        <div v-for="project in projects" class="col-md-6 col-xl-4 col-sm-12 d-flex justify-content-center">
+          <Card :project="project"/>
         </div>
-      </div>
-    </div>  
-  </div>
+      </div>  
+    </div>
   </main>
   
 
@@ -50,4 +41,5 @@ export default{
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
+
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    name: 'ProjectCard',
+    props:{
+        project: Object
+    },
+    data(){
+        return{
+            imgLink: "http://localhost:8000/storage/",
+
+        }
+    }
+}
+</script>
+
+<template>
+
+<div class="card mb-3" style="width: 18rem;">
+    <img v-if="project.image !== null " class="card-img-top" :src="`${imgLink}${project.image}`" :alt="project.title">
+    <div class="card-body">
+        <h5 class="card-title">{{project.title}}</h5>
+        <p class="card-text">{{ project.description }}</p>
+        <a href="#" class="btn btn-primary">vedi progetto</a>
+    </div>
+</div>
+
+</template>
+
+<style scoped lang="scss">
+</style>
