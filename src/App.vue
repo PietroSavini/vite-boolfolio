9aea5e82cbfd42f120931d3b16f0d728859b14a5
PietroSavini <savinipietro@gmail.com>
2023-06-15 18:11:17 +0200

    ADD: connection with laravel-API

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,20 @@ import axios from 'axios';
 export default{
   data(){
     return{
-      projects:[],
+      projects: [],
     }
   },
   mounted(){
     this.getProjects();
+    
   },
 
   methods:{
    
     getProjects(){
-      axios.get('http://localhost:8000/Api/Project').then((resp) =>{
-        this.projects = resp.results;
-        console.log(results);
+      axios.get('http://localhost:8000/api/projects').then((resp) =>{
+        this.projects = resp.data.results;
+        console.log(this.projects);
       });
     }
 
@@ -26,7 +27,11 @@ export default{
 </script>
 
 <template>
-  
+  <div class="container">
+    <div v-for="project in projects" class="card">
+    ciao</div>
+  </div>
+
 </template>
 
 <style scoped lang="scss">
