ca24cc323cfdcdc8e1b1bdea93a64106799deaa0
PietroSavini <savinipietro@gmail.com>
2023-06-15 18:46:51 +0200

    ADD: main content with card from API

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,26 @@ export default{
 </script>
 
 <template>
-  <div class="container">
-    <div v-for="project in projects" class="card">
-    ciao</div>
+  <main>
+    <div class="container py-4">
+    <div class="row">
+      <div v-for="project in projects" class="col-md-6 col-xl-4 col-sm-12 d-flex justify-content-center ">
+        <div class="card mb-3" style="width: 18rem;">
+          <img v-if="project.image !== null " class="card-img-top" :src="project.image" :alt="project.title">
+          <div class="card-body">
+            <h5 class="card-title">{{project.title}}</h5>
+            <p class="card-text">{{ project.description }}</p>
+            <a href="#" class="btn btn-primary">vedi progetto</a>
+          </div>
+        </div>
+      </div>
+    </div>  
   </div>
+  </main>
+  
 
 </template>
 
-<style scoped lang="scss">
-
+<style lang="scss">
+@use "./styles/general.scss" as *;
 </style>
