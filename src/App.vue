c1c52180de78b7cd79493cb481390f1529488e13
PietroSavini <savinipietro@gmail.com>
2023-06-18 17:53:24 +0200

    ADD: pagination

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,27 @@ export default{
         return {
             projects: [],
             imgLink: "http://localhost:8000/storage/",
-            dbUrl: "http://localhost:8000"
+            dbUrl: "http://localhost:8000",
+            currentPage: 1,
+            lastPage:null,
+            totalProjects:0,
+
         };
     },
     mounted() {
         this.getProjects();
     },
     methods: {
-        getProjects() {
-            axios.get(`${this.dbUrl}/api/projects`).then((resp) => {
+        getProjects(page = 1) {
+            axios.get(`${this.dbUrl}/api/projects`,{
+              params:{
+                page : page
+              }
+            }).then((resp) => {
                 this.projects = resp.data.results.data;
+                this.currentPage = resp.data.results.current_page;
+                this.lastPage = resp.data.results.last_page;
+                this.totalProjects = resp.data.results.total;
                 console.log(this.projects);
             });
         }
@@ -29,10 +40,20 @@ export default{
   <main>
     <div class="container py-4">
       <div class="row">
+        <p>totale progetti: {{ totalProjects }}</p>
+        <span>pagina: {{ currentPage }}</span>
         <div v-for="project in projects" class="col-md-6 col-xl-4 col-sm-12 d-flex justify-content-center">
           <Card :project="project"/>
         </div>
       </div>  
+      <!-- pagination -->
+      <nav v-if="lastPage" aria-label="Page navigation example" class=" d-flex justify-content-center">
+        <ul class="pagination">
+          <li class="page-item"><a  @click.prevent="getProjects(currentPage - 1)"  class="page-link" href="#">Previous</a></li>
+          <li v-for="pageNum in lastPage" class="page-item" :class="{ 'active': pageNum === currentPage }" ><a class="page-link" href="#">{{pageNum}}</a></li>
+          <li class="page-item"><a  @click.prevent="getProjects(currentPage + 1)" class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
     </div>
   </main>
   
