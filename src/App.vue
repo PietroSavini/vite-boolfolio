532c7a7901cd1ec861a0a462aa88550d3ae88459
PietroSavini <savinipietro@gmail.com>
2023-06-18 22:03:18 +0200

    ADD: navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,18 @@
 <script >
-import axios from 'axios';
-import Card from './components/card.vue';
+import AppHeader from './components/AppHeader.vue';
+
 
 export default{
     data() {
-        return {
-            projects: [],
-            imgLink: "http://localhost:8000/storage/",
-            dbUrl: "http://localhost:8000",
-            currentPage: 1,
-            lastPage:null,
-            totalProjects:0,
-
-        };
+        return {};
     },
-    mounted() {
-        this.getProjects();
-    },
-    methods: {
-        getProjects(page = 1) {
-            axios.get(`${this.dbUrl}/api/projects`,{
-              params:{
-                page : page
-              }
-            }).then((resp) => {
-                this.projects = resp.data.results.data;
-                this.currentPage = resp.data.results.current_page;
-                this.lastPage = resp.data.results.last_page;
-                this.totalProjects = resp.data.results.total;
-                console.log(this.projects);
-            });
-        }
-    },
-    components: { Card }
+    components: { AppHeader }
 }
 </script>
 
 <template>
-  <main>
-    <div class="container py-4">
-      <div class="row">
-        <div v-for="project in projects" class="col-md-6 col-xl-4 col-sm-12 d-flex justify-content-center">
-          <Card :project="project"/>
-        </div>
-      </div>  
-      <!-- pagination -->
-      <nav v-if="lastPage" aria-label="Page navigation example" class=" d-flex justify-content-center">
-        <ul class="pagination">
-          <li :class="{'disabled' : currentPage === 1}" class="page-item"><a  @click.prevent="getProjects(currentPage - 1)"  class="page-link" href="#">Previous</a></li>
-          <li v-for="pageNum in lastPage" class="page-item" :class="{ 'active': pageNum === currentPage }" ><a  @click.prevent="getProjects(pageNum)"  class="page-link" href="#">{{pageNum}}</a></li>
-          <li :class="{'disabled' : currentPage === lastPage}" class="page-item"><a  @click.prevent="getProjects(currentPage + 1)" class="page-link" href="#">Next</a></li>
-        </ul>
-      </nav>
-      <!-- /pagination -->
-    </div>
-  </main>
-  
+<AppHeader />
+<router-view></router-view>
 
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+<script>
+export default{
+    name: "AppHeader",
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+<nav class="navbar navbar-expand-lg navbar-light bg-light px-2">
+    <div class="container d-flex justify-content-between">
+        <div>
+            <a class="navbar-brand" href="#">Savini Pietro Dev</a>
+        </div>
+        <ul class="">
+            <li >
+            <a  href="#">Home</a>
+            </li>
+            <li >
+            <a  href="#">Projects</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+</template>
+
+<style scoped lang="scss">
+ul{
+    padding: 0;
+    margin: 0;
+    li{
+    display: inline-block;
+    margin-right:10px ;
+        a{
+            text-decoration: none;
+            color: inherit;
+        }
+    }
+}
+</style>
