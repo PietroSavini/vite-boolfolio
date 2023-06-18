2d9439ffff9e660b1ee57c1673943a070f8960b2
PietroSavini <savinipietro@gmail.com>
2023-06-19 00:31:32 +0200

    ADD: connection to AAPI & display of data

--- src/pages/singleProject.vue
+++ src/pages/singleProject.vue
@@ -1,24 +1,36 @@
 <script>
-import Projects from './projects.vue';
+
+import axios from 'axios';
 
 export default{
     name:"singleProject",
     data(){
        return{
-
+        dbUrl: "http://localhost:8000",
+        project : {},
+        imgLink: "http://localhost:8000/storage/"
        }
     },
     mounted(){
-        console.log(this.$route)
+        const currentSlug = this.$route.params.slug;
+        axios.get(`${this.dbUrl}/api/projects/${currentSlug}`).then((resp) => {
+                this.project = resp.data.results;
+                console.log(this.project)
+            });
+    },
+    methods:{
+       
     }
+
 }
 </script>
 
 <template>
-    <div class="container">
-        <h1></h1>
-        <p></p>
-        
+    <div class="container text-center py-5">
+        <img v-if="project.img !== null" class="w-100 mb-3" :src="`${imgLink}${project.image}`" alt="">
+        <h1>{{ project.title }}</h1>
+        <p>{{ project.description }}</p>
+
     </div>
 </template>
 
